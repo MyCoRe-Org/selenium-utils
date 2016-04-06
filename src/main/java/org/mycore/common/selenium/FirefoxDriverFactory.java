a0daeb9a780e7adb587752c3383612f5113425ad
Sebastian Hofmann <hofmann.s@uni-jena.de>
2016-04-06 08:52:06 +0000

    DP-6 #comment run integration tests with german firefox profile
    git-svn-id: file:///Users/thosch/test/mycore-svn/svn/maven/selenium-utils/trunk@34968 7089d063-bc14-496d-ac90-53de1872eb6c

--- src/main/java/org/mycore/common/selenium/FirefoxDriverFactory.java
+++ src/main/java/org/mycore/common/selenium/FirefoxDriverFactory.java
@@ -1,18 +1,29 @@
 package org.mycore.common.selenium;
 
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 public class FirefoxDriverFactory extends DriverFactory {
 
     @Override
     public WebDriver getDriver() {
-        FirefoxDriver firefoxDriver = new FirefoxDriver();
+        FirefoxProfile profile = getFirefoxProfile(Locale.GERMANY);
+        FirefoxDriver firefoxDriver = new FirefoxDriver(profile);
         firefoxDriver.manage().window().setSize(new Dimension(dimX, dimY));
         firefoxDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         return firefoxDriver;
+    }
+
+    private FirefoxProfile getFirefoxProfile(Locale locale) {
+        FirefoxProfile profile = new FirefoxProfile();
+        String formattedLocale = locale.getCountry().isEmpty() ? locale.getLanguage()
+                : locale.getLanguage() + "-" + locale.getCountry().toLowerCase(Locale.ROOT);
+        profile.setPreference("intl.accept_languages", formattedLocale);
+        return profile;
     }
 }
